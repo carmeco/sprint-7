917204286fd4a5984369e2a4ec8c3f655c163717
carme <carmecodina.p@gmail.com>
2021-11-22 17:51:21 +0100

    instalo styled components y creo GlobalStyle

---
+++ src/GlobalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    ul {
+        padding-left: 0;
+    }
+    li {
+        list-style: none;
+    }
+`;
