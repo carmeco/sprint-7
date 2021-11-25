634aa3ef327d79174a4fc4c034be77c2873a1841
carme <carmecodina.p@gmail.com>
2021-11-25 12:37:31 +0100

    exercici 4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Panell } from "./App.styles";
 import CountPanel from "./component/CountPanel";
 
 function App() {
-    //Hooks
-    const [check, setCheck] = useState({
+    //useState hooks
+    const initialCheck = JSON.parse(localStorage.getItem("check")) || {
         website: false,
         seo: false,
         google: false,
-    });
-    const [pags, setPags] = useState(1);
-    const [langs, setLangs] = useState(1);
+    };
+    const initialPags = JSON.parse(localStorage.getItem("pags")) || 1;
+    const initialLangs = JSON.parse(localStorage.getItem("langs")) || 1;
+    const [check, setCheck] = useState(initialCheck);
+    const [pags, setPags] = useState(initialPags);
+    const [langs, setLangs] = useState(initialLangs);
+
+    //useRef hooks
+    const website = useRef(null);
+    const seo = useRef(null);
+    const google = useRef(null);
+
+    //useEffect hook
+    useEffect(() => {
+        check.website && (website.current.checked = true);
+        check.seo && (seo.current.checked = true);
+        check.google && (google.current.checked = true);
+    }, []);
 
     //Events
     const handleChange = (event) => {
@@ -26,6 +41,11 @@ function App() {
     let googlePrice = check.google ? 200 : 0;
     const price = websitePrice + seoPrice + googlePrice;
 
+    //LocalStorage
+    localStorage.setItem("check", JSON.stringify(check));
+    localStorage.setItem("pags", JSON.stringify(pags));
+    localStorage.setItem("langs", JSON.stringify(langs));
+
     return (
         <div>
             <p>Què vols fer?</p>
@@ -35,6 +55,7 @@ function App() {
                         <input
                             id="website"
                             name="website"
+                            ref={website}
                             type="checkbox"
                             onChange={handleChange}
                         ></input>
@@ -62,6 +83,7 @@ function App() {
                         <input
                             id="seo"
                             name="seo"
+                            ref={seo}
                             type="checkbox"
                             onChange={handleChange}
                         ></input>
@@ -71,6 +93,7 @@ function App() {
                         <input
                             id="google"
                             name="google"
+                            ref={google}
                             type="checkbox"
                             onChange={handleChange}
                         ></input>
